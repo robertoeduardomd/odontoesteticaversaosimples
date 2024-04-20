a479441f7c8d66d52bef06acb0c8f7f6c6e5d03d
Roberto Eduardo <robertoemd@gmail.com>
2024-04-19 22:10:44 -0300

    conversao odonto e estetica para react

---
+++ odontoeestetica/public/slide.js
@@ -0,0 +1,17 @@
+var radio = document.querySelector('.manual-btn');
+var cont = 1;
+document.getElementById('radio1').checked = true;
+
+function proximaimg(){
+    cont++ ;
+    if (cont > 3){
+        cont = 1;
+    } 
+    document.getElementById('radio'+cont).checked = true;
+  
+
+}
+
+setInterval(() => {
+    proximaimg();
+},2000)
